fd7907ed4b720576c10d9caf7743bb272b4fa902
Adrian Grimm <usmcamgrimm@gmail.com>
2019-08-29 12:34:02 -0400

    Update completed page

--- js/work.js
+++ js/work.js
@@ -1,15 +1,16 @@
-$('.toggle').click(function(e) {
-  	e.preventDefault();
-  
-    var $this = $(this);
-  
-    if ($this.next().hasClass('show')) {
-        $this.next().removeClass('show');
-        $this.next().slideUp(150);
-    } else {
-        $this.parent().parent().find('li .inner').removeClass('show');
-        $this.parent().parent().find('li .inner').slideUp(150);
-        $this.next().toggleClass('show');
-        $this.next().slideToggle(150);
-    }
+$('.accordion').click(function(e) {
+  e.preventDefault();
+  e.collapsible: true
+
+  var $this = $(this);
+
+  if ($this.next().hasClass('show')) {
+      $this.next().removeClass('show');
+      $this.next().slideUp(150);
+  } else {
+      $this.parent().parent().find('workList').removeClass('show');
+      $this.parent().parent().find('workList').slideUp(150);
+      $this.next().toggleClass('show');
+      $this.next().slideToggle(150);
+  }
 });
